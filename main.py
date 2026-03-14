0724dbd51646e775e89a505a122cb90b447f6554
Diego Ruiz <dr1116uiz@gmail.com>
2026-03-13 19:28:23 -0500

    Basic UI - added side bar with Zoom in/out functions - added Ole Miss colors into .kv file

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from kivy_garden.mapview import MapView, MapMarker
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.lang import Builder
 from kivy.app import App
+
+Builder.load_file("parking-app.kv")
+
+class MapScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MapScreen, self).__init__(**kwargs)
 
 class MainApp(App):
     def build(self):
-        map = MapView(zoom=15,lon=-89.538, lat=34.365) # University, MS
-        m1 = MapMarker(lon=-89.5375, lat=34.3709) # Stockard Hall
-        m2 = MapMarker(lon=-89.5375, lat=34.3662) # Weir Hall
-        map.add_marker(m1)
-        map.add_marker(m2)
-        return map
-
-def main():
-    MainApp().run()
+        # m1 = MapMarker(lon=-89.5375, lat=34.3709) # Stockard Hall
+        # m2 = MapMarker(lon=-89.5375, lat=34.3662) # Weir Hall
+        return MapScreen(name="map_screen")
 
 if __name__ == "__main__":
-    main()
+    MainApp().run()
